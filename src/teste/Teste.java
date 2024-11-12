be45fe98b8df7bed757392611cdb51504c71be0f
Rafassz <rafassz.aps@gmail.com>
2024-11-12 10:23:17 -0300

    testando o teste

---
+++ src/teste/Teste.java
@@ -0,0 +1,19 @@
+package teste;
+
+public class Teste {
+	
+	private String teste;
+
+	public String getTeste() {
+		return teste;
+	}
+
+	public void setTeste(String teste) {
+		this.teste = teste;
+	}
+
+	public Teste(String teste) {
+		this.teste = teste;
+	}
+
+}
